ebb3bd3bba4c96c9072b2ba4e075ce16bed0af9c
domingos <mds.junior87@gmail.com>
2024-05-21 22:52:30 -0300

    correção

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,34 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+       // String nomeCompleto = "LINGUAGEM " + "JAVA";
+          
+        //qual o resultado das expressoes abaixo?
+        
+        String concatenacao ="?"; 
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+
+    }
+}
