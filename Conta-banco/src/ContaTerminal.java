aeba36347c1cc9f7692dfbf2a0bf13ca70011073
domingos <mds.junior87@gmail.com>
2024-05-28 18:25:24 -0300

    desafio conta banco

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Random;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Random random = new Random();
+
+        //Exibir as mensagens para o usuário
+        System.out.println("Bem vindo ao Banco Nosso Banco!");
+
+        //Obter pela scanner os valores digitados no terminal
+        System.out.println("Digite seu nome complete para abertura de conta: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        int numeroConta = 100000 + random.nextInt(900000);
+
+        double saldo = 237.48;
+
+        //Exibir a mensagem final da conta criadajuca 
+        System.out.println("Olá,"+nome+" obrigado por criar uma conta em nosso banco, sua agência é "+agencia+" conta "+numeroConta+" e seu saldo de "+ saldo +" já está disponível para saque");
+
+
+
     }
 }
